2409a27fc1879d1d48eaa66970346bd2d17353c5
rihan2575 <83025633+rihan2575@users.noreply.github.com>
2021-05-15 10:37:47 +0530

    Create __main__.py

---
+++ ShinchanMusic/__main__.py
@@ -0,0 +1,16 @@
+from pyrogram import Client as Bot
+
+from ShinchanMusic.config import API_HASH, API_ID, BOT_TOKEN
+from ShinchanMusic.services.callsmusic import run
+
+# os.system(f"wget -O ./etc/foreground.png {BG_IMAGE}")
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="DaisyXMusic.modules"),
+)
+
+bot.start()
+run()
