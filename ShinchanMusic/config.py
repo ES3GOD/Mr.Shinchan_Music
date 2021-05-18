7ab420ff2937a087d0145eb8205c7a0eae74b69f
rihan2575 <83025633+rihan2575@users.noreply.github.com>
2021-05-18 11:37:35 +0530

    Update config.py

--- ShinchanMusic/config.py
+++ ShinchanMusic/config.py
@@ -15,8 +15,12 @@ BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/544da923c008df67f98f4.png
 admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Shinchan_Helper")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "Shinchan_Support")
+PROJECT_NAME = getenv("PROJECT_NAME", "ShinchanMusic")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/rihan2575/Mr.Shinchan_Music")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
