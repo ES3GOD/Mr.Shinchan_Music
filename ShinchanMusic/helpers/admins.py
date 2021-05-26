e67d82e6aa362fd2a181a08aeb804a8912e49b25
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-05-26 09:51:04 +0530

    Update admins.py

--- ShinchanMusic/helpers/admins.py
+++ ShinchanMusic/helpers/admins.py
@@ -6,17 +6,18 @@ from ShinchanMusic.function.admins import get as gett
 from ShinchanMusic.function.admins import set
 
 
-async def get_administrators(chat: Chat) -> List[int]:
-    get = admins.get(chat.id)
+async def get_administrators(chat: Chat) -> List[User]:
+    get = gett(chat.id)
+
     if get:
         return get
     else:
-        administrators = await chat.get_members(filter='administrators')
+        administrators = await chat.get_members(filter="administrators")
         to_set = []
 
         for administrator in administrators:
-            if administrator.can_manage_voice_chats:
-                to_set.append(administrator.user.id)
+            # if administrator.can_manage_voice_chats:
+            to_set.append(administrator.user.id)
 
-        admins.set(chat.id, to_set)
+        set(chat.id, to_set)
         return await get_administrators(chat)
