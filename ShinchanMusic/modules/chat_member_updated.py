87bef1cecf45f133eb7d94cdcfaaceec57c8a52a
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-05-29 19:37:05 +0530

    Q

--- ShinchanMusic/modules/chat_member_updated.py
+++ ShinchanMusic/modules/chat_member_updated.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pyrogram.types import ChatMemberUpdated
 
-from ShinchanMusic.function import admins
+from ShinchanMusic.function import *
 
 
 @Client.on_chat_member_updated()
