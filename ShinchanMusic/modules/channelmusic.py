870e28f1c95dd9b9e6612b87e3caac4f882ddd69
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-06-08 19:02:07 +0530

    Update channelmusic.py

--- ShinchanMusic/modules/channelmusic.py
+++ ShinchanMusic/modules/channelmusic.py
@@ -34,7 +34,8 @@ from ShinchanMusic.helpers.channelmusic import get_chat_id
 from ShinchanMusic.helpers.decorators import authorized_users_only
 from ShinchanMusic.helpers.filters import command, other_filters
 from ShinchanMusic.helpers.gets import get_file_name
-from ShinchanMusic.services.callsmusic import callsmusic, queues
+from ShinchanMusic.services.callsmusic import callsmusic
+from ShinchanMusic.services.queues import queues
 from ShinchanMusic.services.callsmusic.callsmusic import client as USER
 from ShinchanMusic.services.converter.converter import convert
 from ShinchanMusic.services.downloaders import youtube
