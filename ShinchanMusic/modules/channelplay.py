c553fd60ad6566a05e4318526db8c5233ba74954
Shinchan Helper <84756205+Shinchan-Helper@users.noreply.github.com>
2021-06-12 23:37:37 +0530

    Q

--- ShinchanMusic/modules/channelplay.py
+++ ShinchanMusic/modules/channelplay.py
@@ -35,10 +35,10 @@ from ShinchanMusic.helpers.decorators import authorized_users_only
 from ShinchanMusic.helpers.filters import command, other_filters
 from ShinchanMusic.helpers.gets import get_file_name
 from ShinchanMusic.services.callsmusic import callsmusic
-from ShinchanMusic.services.queues import queues
 from ShinchanMusic.services.callsmusic.callsmusic import client as USER
 from ShinchanMusic.services.converter.converter import convert
 from ShinchanMusic.services.downloaders import youtube
+from ShinchanMusic.services.queues import queues
 
 chat_id = None
 
